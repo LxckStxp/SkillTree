6813ad077287deb3f7fb8a4a2c13b8379eb2f63f
LxckStxp <lxckstxp@gmail.com>
2025-01-27 19:24:11 +1000

    Create CoreUtils.lua

---
+++ Modules/CoreUtils.lua
@@ -0,0 +1,9 @@
+-- CoreUtils.lua
+
+local CoreUtils = {}
+
+function CoreUtils.Init(SkillTree) SkillTree.GlobalData.SomeImportantData = "Initialized" end
+
+function CoreUtils.UtilityFunction() SkillTree.Logger.Log("CoreUtils", "Utility function called"); return "Utility function result" end
+
+return CoreUtils
