bd4670e3068afad4cd19da3f79ca6580201722aa
LxckStxp <lxckstxp@gmail.com>
2025-01-27 21:01:43 +1000

    Update MainScript.lua

--- MainScript.lua
+++ MainScript.lua
@@ -9,12 +9,11 @@ local ModuleLoader = loadstring(game:HttpGet("https://raw.githubusercontent.com/
 ModuleLoader.LoadModule("ModuleLoader", "https://raw.githubusercontent.com/LxckStxp/SkillTree/main/ModuleLoader.lua", _G.SkillTree)
 
 local function InitSkillTree()
-    -- Load core modules
+    -- Load core modules (except UIHandler)
     ModuleLoader.LoadModule("SharedConfig", "https://raw.githubusercontent.com/LxckStxp/SkillTree/main/CoreModules/SharedConfig.lua", _G.SkillTree)
     ModuleLoader.LoadModule("Logger", "https://raw.githubusercontent.com/LxckStxp/SkillTree/main/CoreModules/Logger.lua", _G.SkillTree)
     ModuleLoader.LoadModule("CoreUtils", "https://raw.githubusercontent.com/LxckStxp/SkillTree/main/Modules/CoreUtils.lua", _G.SkillTree)
     ModuleLoader.LoadModule("NetworkManager", "https://raw.githubusercontent.com/LxckStxp/SkillTree/main/Modules/NetworkManager.lua", _G.SkillTree)
-    ModuleLoader.LoadModule("UIHandler", "https://raw.githubusercontent.com/LxckStxp/SkillTree/main/UI/UIHandler.lua", _G.SkillTree)
     
     -- Load plugins
     local plugins = {"ActionPlugin", "JumpPlugin"}
@@ -22,9 +21,17 @@ local function InitSkillTree()
         ModuleLoader.LoadModule(pluginName, "https://raw.githubusercontent.com/LxckStxp/SkillTree/main/Plugins/" .. pluginName .. ".lua", _G.SkillTree)
     end
     
-    -- Initialize all modules
+    -- Initialize all modules (except UIHandler)
     for _, module in pairs(_G.SkillTree.Modules) do
         if module.OnStart then module.OnStart(_G.SkillTree) end
+    end
+    
+    -- Load UIHandler last
+    ModuleLoader.LoadModule("UIHandler", "https://raw.githubusercontent.com/LxckStxp/SkillTree/main/UI/UIHandler.lua", _G.SkillTree)
+    
+    -- Initialize UIHandler
+    if _G.SkillTree.UIHandler and _G.SkillTree.UIHandler.OnStart then
+        _G.SkillTree.UIHandler.OnStart(_G.SkillTree)
     end
 end
 
