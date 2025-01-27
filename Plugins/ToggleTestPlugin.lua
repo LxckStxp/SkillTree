c0a7b09dc98e65414c914dde3ab4cc1941d4ac4c
LxckStxp <lxckstxp@gmail.com>
2025-01-27 22:46:36 +1000

    Update ToggleTestPlugin.lua

--- Plugins/ToggleTestPlugin.lua
+++ Plugins/ToggleTestPlugin.lua
@@ -7,18 +7,29 @@ ToggleTestPlugin.IsPlugin = true
 local toggleState = false
 
 function ToggleTestPlugin.GetContent(SkillTree)
-    return {
-        SkillTree.UI.Elements.CreateToggleButton(SkillTree, nil, "Toggle Test", function()
-            toggleState = not toggleState
-            print("ToggleTestPlugin: Attempting to call UpdateToggleButton")
-            if SkillTree.UIHandler and SkillTree.UIHandler.UpdateToggleButton then
-                print("ToggleTestPlugin: UIHandler and UpdateToggleButton found")
-                SkillTree.UIHandler.UpdateToggleButton(SkillTree, "Toggle Test", toggleState)
-            else
-                warn("ToggleTestPlugin: UIHandler or UpdateToggleButton not found")
-            end
-        end, toggleState)
-    }
+    print("ToggleTestPlugin: Entering GetContent")
+    print("ToggleTestPlugin: SkillTree: " .. tostring(SkillTree))
+    print("ToggleTestPlugin: SkillTree.UI: " .. tostring(SkillTree.UI))
+    print("ToggleTestPlugin: SkillTree.UI.Elements: " .. tostring(SkillTree.UI and SkillTree.UI.Elements))
+    print("ToggleTestPlugin: SkillTree.UI.Elements.CreateToggleButton: " .. tostring(SkillTree.UI and SkillTree.UI.Elements and SkillTree.UI.Elements.CreateToggleButton))
+    
+    if SkillTree.UI and SkillTree.UI.Elements and SkillTree.UI.Elements.CreateToggleButton then
+        return {
+            SkillTree.UI.Elements.CreateToggleButton(SkillTree, nil, "Toggle Test", function()
+                toggleState = not toggleState
+                print("ToggleTestPlugin: Attempting to call UpdateToggleButton")
+                if SkillTree.UIHandler and SkillTree.UIHandler.UpdateToggleButton then
+                    print("ToggleTestPlugin: UIHandler and UpdateToggleButton found")
+                    SkillTree.UIHandler.UpdateToggleButton(SkillTree, "Toggle Test", toggleState)
+                else
+                    warn("ToggleTestPlugin: UIHandler or UpdateToggleButton not found")
+                end
+            end, toggleState)
+        }
+    else
+        warn("ToggleTestPlugin: UIElements or CreateToggleButton not found")
+        return {}
+    end
 end
 
 return ToggleTestPlugin
