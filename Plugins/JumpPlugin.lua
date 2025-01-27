74e25d173f5e6b2bd06d78f3d372d14370d843b5
LxckStxp <lxckstxp@gmail.com>
2025-01-27 21:14:41 +1000

    Update JumpPlugin.lua

--- Plugins/JumpPlugin.lua
+++ Plugins/JumpPlugin.lua
@@ -7,35 +7,37 @@ local JumpPlugin = {
 function JumpPlugin.Init(SkillTree)
     SkillTree.Logger.Log("JumpPlugin", "Initialized")
     
-    -- Set up jump event listener
-    local function setupJumpListener()
-        local player = game:GetService("Players").LocalPlayer
-        if player.Character then
-            local humanoid = player.Character:FindFirstChildOfClass("Humanoid")
-            if humanoid then
-                humanoid.Jumping:Connect(function()
-                    SkillTree.Logger.Log("JumpPlugin", "Player jumped!")
-                end)
-            end
-        end
-        
-        player.CharacterAdded:Connect(function(character)
-            local humanoid = character:WaitForChild("Humanoid")
+    local function setupJumpListener(character)
+        local humanoid = character:FindFirstChildOfClass("Humanoid")
+        if humanoid then
             humanoid.Jumping:Connect(function()
                 SkillTree.Logger.Log("JumpPlugin", "Player jumped!")
             end)
-        end)
+        else
+            character:WaitForChild("Humanoid").Jumping:Connect(function()
+                SkillTree.Logger.Log("JumpPlugin", "Player jumped!")
+            end)
+        end
+    end
+    
+    local function setupPlayerListener(player)
+        if player.Character then
+            setupJumpListener(player.Character)
+        end
+        
+        player.CharacterAdded:Connect(setupJumpListener)
     end
     
     -- Set up the listener immediately if the player exists
-    local player = game:GetService("Players").LocalPlayer
-    if player then
-        setupJumpListener()
+    local players = game:GetService("Players")
+    local localPlayer = players.LocalPlayer
+    if localPlayer then
+        setupPlayerListener(localPlayer)
     else
         -- If the player doesn't exist yet, wait for them to load
-        game:GetService("Players").PlayerAdded:Connect(function(newPlayer)
-            if newPlayer == game:GetService("Players").LocalPlayer then
-                setupJumpListener()
+        players.PlayerAdded:Connect(function(newPlayer)
+            if newPlayer == players.LocalPlayer then
+                setupPlayerListener(newPlayer)
             end
         end)
     end
