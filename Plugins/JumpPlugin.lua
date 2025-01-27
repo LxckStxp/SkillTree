6713910b0a960b003e17bb310233a12b4f7fe3e2
LxckStxp <lxckstxp@gmail.com>
2025-01-27 21:11:41 +1000

    Update JumpPlugin.lua

--- Plugins/JumpPlugin.lua
+++ Plugins/JumpPlugin.lua
@@ -8,12 +8,37 @@ function JumpPlugin.Init(SkillTree)
     SkillTree.Logger.Log("JumpPlugin", "Initialized")
     
     -- Set up jump event listener
-    game:GetService("Players").LocalPlayer.CharacterAdded:Connect(function(character)
-        local humanoid = character:WaitForChild("Humanoid")
-        humanoid.Jumping:Connect(function()
-            SkillTree.Logger.Log("JumpPlugin", "Player jumped!")
+    local function setupJumpListener()
+        local player = game:GetService("Players").LocalPlayer
+        if player.Character then
+            local humanoid = player.Character:FindFirstChildOfClass("Humanoid")
+            if humanoid then
+                humanoid.Jumping:Connect(function()
+                    SkillTree.Logger.Log("JumpPlugin", "Player jumped!")
+                end)
+            end
+        end
+        
+        player.CharacterAdded:Connect(function(character)
+            local humanoid = character:WaitForChild("Humanoid")
+            humanoid.Jumping:Connect(function()
+                SkillTree.Logger.Log("JumpPlugin", "Player jumped!")
+            end)
         end)
-    end)
+    end
+    
+    -- Set up the listener immediately if the player exists
+    local player = game:GetService("Players").LocalPlayer
+    if player then
+        setupJumpListener()
+    else
+        -- If the player doesn't exist yet, wait for them to load
+        game:GetService("Players").PlayerAdded:Connect(function(newPlayer)
+            if newPlayer == game:GetService("Players").LocalPlayer then
+                setupJumpListener()
+            end
+        end)
+    end
 end
 
 function JumpPlugin.GetContent(SkillTree)
