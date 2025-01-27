a21e214389a7f84758b499616395cadd1706c07a
LxckStxp <lxckstxp@gmail.com>
2025-01-27 19:24:38 +1000

    Create NetworkManager.lua

---
+++ Modules/NetworkManager.lua
@@ -0,0 +1,11 @@
+-- NetworkManager.lua
+
+local NetworkManager = {}
+
+function NetworkManager.Init(SkillTree) SkillTree.GlobalData.NetworkStatus = "Connected" end
+
+function NetworkManager.SendData(data) SkillTree.Logger.Log("NetworkManager", "Sending data: " .. data) end
+
+function NetworkManager.ReceiveData() SkillTree.Logger.Log("NetworkManager", "Receiving data"); return "Received data" end
+
+return NetworkManager
