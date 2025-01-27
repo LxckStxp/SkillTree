693901b542924f35de9c934fddc08be36d0b1cf8
LxckStxp <lxckstxp@gmail.com>
2025-01-27 19:37:21 +1000

    Update NetworkManager.lua

--- Modules/NetworkManager.lua
+++ Modules/NetworkManager.lua
@@ -9,4 +9,3 @@ function NetworkManager.SendData(data) SkillTree.Logger.Log("NetworkManager", "S
 function NetworkManager.ReceiveData() SkillTree.Logger.Log("NetworkManager", "Receiving data"); return "Received data" end
 
 return NetworkManager
- 
