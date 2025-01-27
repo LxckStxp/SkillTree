4de73cf20912f7f4c82b70fa1a6d0e9c8f5b97a3
LxckStxp <lxckstxp@gmail.com>
2025-01-27 22:28:53 +1000

    Update ToggleTestPlugin.lua

--- Plugins/ToggleTestPlugin.lua
+++ Plugins/ToggleTestPlugin.lua
@@ -10,7 +10,11 @@ function ToggleTestPlugin.GetContent(SkillTree)
     return {
         SkillTree.UI.Elements.CreateButton(SkillTree, nil, "Toggle Test", function()
             toggleState = not toggleState
-            UIHandler.UpdateToggleButton(SkillTree, "Toggle Test", toggleState)
+            if SkillTree.UIHandler and SkillTree.UIHandler.UpdateToggleButton then
+                SkillTree.UIHandler.UpdateToggleButton(SkillTree, "Toggle Test", toggleState)
+            else
+                warn("UIHandler or UpdateToggleButton not found")
+            end
         end)
     }
 end
