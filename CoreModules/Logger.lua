570b7eed94314d43461281b8b7336c5f86c33664
LxckStxp <lxckstxp@gmail.com>
2025-01-27 19:25:37 +1000

    Update Logger.lua

--- CoreModules/Logger.lua
+++ CoreModules/Logger.lua
@@ -2,9 +2,9 @@
 
 local Logger = {}
 
-function Logger.Log(module, msg) print(string.format("[%s] [%s] %s", os.date("%Y-%m-%d %H:%M:%S"), module, msg)) end
-function Logger.Warn(module, msg) warn(string.format("[%s] [%s] WARNING: %s", os.date("%Y-%m-%d %H:%M:%S"), module, msg)) end
-function Logger.Error(module, msg) error(string.format("[%s] [%s] ERROR: %s", os.date("%Y-%m-%d %H:%M:%S"), module, msg)) end
+function Logger.Log(module, msg) print(string.format("[%s] %s", module, msg)) end
+function Logger.Warn(module, msg) warn(string.format("[%s] WARNING: %s", module, msg)) end
+function Logger.Error(module, msg) error(string.format("[%s] ERROR: %s", module, msg)) end
 
 function Logger.Init(SkillTree) SkillTree.Logger = Logger end
 
