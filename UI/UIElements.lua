e2ec650656ad28b0feec9b35a9182e95fda0b135
LxckStxp <lxckstxp@gmail.com>
2025-01-27 19:47:55 +1000

    Update UIElements.lua

--- UI/UIElements.lua
+++ UI/UIElements.lua
@@ -1,11 +1,42 @@
--- UIElements.lua
+-- main/UI/UIElements.lua
 
 local UIElements = {}
 
-function UIElements.Create(type, props)
-    local element = Instance.new(type)
-    for k, v in pairs(props) do element[k] = v end
-    return element
+function UIElements.CreateFrame(SkillTree)
+    return Instance.new("Frame", game.Players.LocalPlayer.PlayerGui) do
+        Name = "SkillTreeMainFrame"
+        Size = SkillTree.SharedConfig.GetConfig("UI", "Size")
+        Position = SkillTree.SharedConfig.GetConfig("UI", "Position")
+        BackgroundColor3 = Color3.new(0, 0, 0)
+        BackgroundTransparency = 0.5
+    end
+end
+
+function UIElements.CreateTitleLabel(SkillTree, parent)
+    return Instance.new("TextLabel", parent) do
+        Name = "SkillTreeTitle"
+        Size = UDim2.new(1, 0, 0, 30)
+        Position = UDim2.new(0, 0, 0, 0)
+        BackgroundColor3 = Color3.new(1, 1, 1)
+        Text = "SkillTree"
+        TextColor3 = Color3.new(0, 0, 0)
+        Font = SkillTree.SharedConfig.GetConfig("UI", "Font")
+        TextSize = SkillTree.SharedConfig.GetConfig("UI", "FontSize")
+    end
+end
+
+function UIElements.CreateButton(SkillTree, parent, text, onClick)
+    return Instance.new("TextButton", parent) do
+        Name = text .. "Button"
+        Size = UDim2.new(0, 100, 0, 30)
+        Position = UDim2.new(0.5, -50, 0.5, 0)
+        BackgroundColor3 = Color3.new(0.5, 0.5, 0.5)
+        Text = text
+        TextColor3 = Color3.new(1, 1, 1)
+        Font = Enum.Font.SourceSans
+        TextSize = 14
+        MouseButton1Click:Connect(onClick)
+    end
 end
 
 return UIElements
