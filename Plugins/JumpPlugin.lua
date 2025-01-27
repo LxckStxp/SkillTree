093c29b77bb66c64e5e29f23e64548673538fad4
LxckStxp <lxckstxp@gmail.com>
2025-01-27 20:39:00 +1000

    Create JumpPlugin.lua

---
+++ Plugins/JumpPlugin.lua
@@ -0,0 +1,24 @@
+-- main/Plugins/JumpPlugin.lua
+
+local JumpPlugin = {}
+
+function JumpPlugin.Init(SkillTree)
+    SkillTree.Logger.Log("JumpPlugin", "Initialized")
+    
+    -- Set up jump event listener
+    game:GetService("Players").LocalPlayer.CharacterAdded:Connect(function(character)
+        local humanoid = character:WaitForChild("Humanoid")
+        humanoid.Jumping:Connect(function()
+            SkillTree.Logger.Log("JumpPlugin", "Player jumped!")
+        end)
+    end)
+end
+
+function JumpPlugin.GetContent(SkillTree)
+    return {
+        "This plugin prints to the console when you jump.",
+        "No buttons needed!"
+    }
+end
+
+return JumpPlugin
