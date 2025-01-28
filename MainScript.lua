4c9efc88bb0fb9e7a468c5f2790e829064467d0d
LxckStxp <lxckstxp@gmail.com>
2025-01-28 12:42:21 +1000

    Update MainScript.lua

--- MainScript.lua
+++ MainScript.lua
@@ -3,15 +3,15 @@
 _G.SkillTree = {Modules = {}, GlobalData = {}}
 
 -- Load ModuleLoader
-local ModuleLoader = loadstring(game:HttpGet("https://raw.githubusercontent.com/LxckStxp/SkillTree/main/ModuleLoader.lua"))()
+local ModuleLoader = loadstring(game:HttpGet("https://raw.githubusercontent.com/LcxkStxp/SkillTree/main/ModuleLoader.lua"))()
 
 -- Use ModuleLoader to load itself into SkillTree
-ModuleLoader.LoadModule("ModuleLoader", "https://raw.githubusercontent.com/LxckStxp/SkillTree/main/ModuleLoader.lua", _G.SkillTree)
+ModuleLoader.LoadModule("ModuleLoader", "https://raw.githubusercontent.com/LcxkStxp/SkillTree/main/ModuleLoader.lua", _G.SkillTree)
 
 local function InitSkillTree()
     -- Load core modules
-    ModuleLoader.LoadModule("SharedConfig", "https://raw.githubusercontent.com/LxckStxp/SkillTree/main/CoreModules/SharedConfig.lua", _G.SkillTree)
-    ModuleLoader.LoadModule("Logger", "https://raw.githubusercontent.com/LxckStxp/SkillTree/main/CoreModules/Logger.lua", _G.SkillTree)
+    ModuleLoader.LoadModule("SharedConfig", "https://raw.githubusercontent.com/LcxkStxp/SkillTree/main/CoreModules/SharedConfig.lua", _G.SkillTree)
+    ModuleLoader.LoadModule("Logger", "https://raw.githubusercontent.com/LcxkStxp/SkillTree/main/CoreModules/Logger.lua", _G.SkillTree)
     
     -- Initialize core modules
     for _, module in pairs(_G.SkillTree.Modules) do
@@ -21,12 +21,12 @@ local function InitSkillTree()
     end
     
     -- Load UIElements
-    ModuleLoader.LoadModule("UIElements", "https://raw.githubusercontent.com/LxckStxp/SkillTree/main/UI/UIElements.lua", _G.SkillTree)
+    ModuleLoader.LoadModule("UIElements", "https://raw.githubusercontent.com/LcxkStxp/SkillTree/main/UI/UIElements.lua", _G.SkillTree)
     
     -- Load plugins
     local plugins = {"ActionPlugin", "JumpPlugin", "ToggleTestPlugin"}
     for _, pluginName in ipairs(plugins) do
-        ModuleLoader.LoadModule(pluginName, "https://raw.githubusercontent.com/LxckStxp/SkillTree/main/Plugins/" .. pluginName .. ".lua", _G.SkillTree)
+        ModuleLoader.LoadModule(pluginName, "https://raw.githubusercontent.com/LcxkStxp/SkillTree/main/Plugins/" .. pluginName .. ".lua", _G.SkillTree)
     end
     
     -- Initialize plugins
@@ -37,13 +37,12 @@ local function InitSkillTree()
     end
     
     -- Load UIHandler last
-    ModuleLoader.LoadModule("UIHandler", "https://raw.githubusercontent.com/LxckStxp/SkillTree/main/UI/UIHandler.lua", _G.SkillTree)
+    ModuleLoader.LoadModule("UIHandler", "https://raw.githubusercontent.com/LcxkStxp/SkillTree/main/UI/UIHandler.lua", _G.SkillTree)
     
     -- Initialize UIHandler
-    if _G.SkillTree.UIHandler and _G.SkillTree.UIHandler.OnStart then
-        _G.SkillTree.UIHandler.OnStart(_G.SkillTree)
+    if _G.SkillTree.UIHandler and _G.SkillTree.UIHandler.Init then
+        _G.SkillTree.UIHandler.Init(_G.SkillTree)
     end
 end
 
 InitSkillTree()
- 
