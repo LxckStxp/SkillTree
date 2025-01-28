2d24428c22415421e831cff8e00b78ebe0164c4e
LxckStxp <lxckstxp@gmail.com>
2025-01-28 10:23:26 +1000

    Delete Modules/CoreUtils.lua

--- Modules/CoreUtils.lua
+++
@@ -1,9 +0,0 @@
--- CoreUtils.lua
-
-local CoreUtils = {}
-
-function CoreUtils.Init(SkillTree) SkillTree.GlobalData.SomeImportantData = "Initialized" end
-
-function CoreUtils.UtilityFunction() return "Utility function result" end
-
-return CoreUtils
